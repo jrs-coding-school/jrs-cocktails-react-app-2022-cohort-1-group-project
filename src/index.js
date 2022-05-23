cbdbd04408fcd9429afcba304ec11fc36f29d3a2
kristinatrainer <100874466+kristinatrainer@users.noreply.github.com>
2022-05-23 11:44:26 -0400

    Set up routing, installed react router dom

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UserLogin  from './components/login/UserLogin'
+import UserSignUp from './components/login/UserSignUp';
+import IngredientsLandingPage from './components/ingredientsLandingPage/IngredientsLandingPage';
+import IndividualDrinkPage from './components/individualDrinkPage/IndividualDrinkPage';
+import MyDrinksPage from './components/myDrinks/MyDrinksPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+          <Route path="/login" element={<UserLogin />}></Route>
+          <Route path="/signup" element={<UserSignUp />}></Route>
+          <Route path="/cocktails" element={<IngredientsLandingPage />}></Route>
+          <Route path="/cocktail/:id" element={<IndividualDrinkPage />}></Route>
+          <Route path="/my-drinks" element={<MyDrinksPage />}></Route>
+          <Route path="*" element={<div>404 - page does not exist</div>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   </React.StrictMode>
 );
 
