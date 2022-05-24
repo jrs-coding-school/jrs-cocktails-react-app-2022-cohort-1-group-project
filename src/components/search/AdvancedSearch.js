0aa926e88c327ecc7760509a7000dea19b4ec4be
bferri42 <79776968+bferri42@users.noreply.github.com>
2022-05-24 14:00:17 -0400

    all changes

---
+++ src/components/search/AdvancedSearch.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './AdvancedSearch.css'
+
+
+
+export default function AdvancedSearch() {
+    
+   
+
+    return (
+        <form className='ingredients-form'>
+            
+            {/* <label htmlFor="ingredient">
+                Search for Ingredient:
+            </label>
+       
+            <select>
+            <option name="vodka"> Vodka</option>
+            <option name="rum">Rum</option>
+            <option name="tequila">Tequila</option>
+            <option name="whiskey">Whiskey</option>
+            <option name="wine">Wine</option>
+            <option name="gin">Gin</option>
+            <option name="cognac">Cognac</option>
+            </select>
+            <input
+                type="text"
+                name="ingredient"
+
+                // value={form.username}
+                // onChange={onInputChanged}
+
+                id="ingredient"
+                placeholder="Enter Ingredient"
+                required
+            />
+            <button type='submit'>Submit</button> */}
+
+        </form>
+
+    )
+}
