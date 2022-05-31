50852b691ebdd0f4d2347e55a44eb0c793b80cee
kglozoya <101759059+kglozoya@users.noreply.github.com>
2022-05-31 14:00:33 -0400

    random button on home page working

--- src/App.js
+++ src/App.js
@@ -5,21 +5,36 @@ import RandomCocktailGenerator from './components/randomCocktailGenerator/Random
 import Footer from './components/footer/Footer';
 import NavBar from './components/navBar/NavBar';
 import { useState } from 'react';
+import { useAxios } from './services/axios.service';
+import { useNavigate } from 'react-router-dom';
 
-function App() {
+function App () {
 
-  const [visible, setVisible] = useState(true);
+  const [ visible, setVisible ] = useState( true );
+  const http = useAxios();
+  const navigate = useNavigate();
 
-
+function onHandleClicked(){
+  http.getRandomDrink()
+  .then((results)=>{
+    var idDrink = results.data.drink[0].idDrink
+    navigate(`/cocktail/${idDrink}`)
+  })  
+}
 
   return (
     <div className="App">
-      <div className="logo"><b>Sip, sip, <br/> hooray!</b></div>
+      <div className="logo"><b>Sip, sip, <br /> hooray!</b></div>
 
       {visible ? <DrinkSearch /> : <AdvancedSearch />}
-    
-      {visible ? <button className="toggle two" onClick={() => setVisible(!visible)}>Click here to Search by Ingredients</button> 
-      : <button className="toggle one" onClick={() => setVisible(!visible)}>Click here to Search by Cocktail</button>}
+
+      {visible ? <button className="toggle two" onClick={() => setVisible( !visible )}>Click here to Search by Ingredients</button>
+        : <button className="toggle one" onClick={() => setVisible( !visible )}>Click here to Search by Cocktail</button>}
+      <button 
+      className='random-drink-button'
+      onClick={onHandleClicked}
+      >Shake it up!</button>
+
       {/* <RandomCocktailGenerator /> */}
       <Footer />
     </div>
--- src/components/randomCocktailGenerator/RandomCocktailGenerator.js
+++ src/components/randomCocktailGenerator/RandomCocktailGenerator.js
@@ -4,8 +4,7 @@ import './RandomCocktailGenerator.css'
 export default function RandomCocktailGenerator() {
   return (
     <div className='random-drink'>
-      <h1>Make Me a Random Drink!</h1>
-      <button className='random-drink-button'>Random Drink!</button>
+      <button className='random-drink-button'>Shake it up!</button>
     </div>
   )
 }
